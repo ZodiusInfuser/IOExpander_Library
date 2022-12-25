2f102e21cbe9daa341f99469f553512064ad7407
Tim Lunn <tim@feathertop.org>
2022-12-25 23:37:36 +1100

    Add Heater and RGB control to ESPHome example

--- examples/esphome/mics6814.h
+++ examples/esphome/mics6814.h
@@ -10,31 +10,27 @@ static const unsigned int DELAY_MS = 15 * 1000;
 GasBreakout gas(Wire, 0x19);
 
 class mics6814 : public PollingComponent {
- public:
-  Sensor *red_sensor = new Sensor();
-  Sensor *nh3_sensor = new Sensor();
-  Sensor *ox_sensor = new Sensor();
+  public:
+    Sensor *red_sensor = new Sensor();
+    Sensor *nh3_sensor = new Sensor();
+    Sensor *ox_sensor = new Sensor();
 
-  mics6814() : PollingComponent(DELAY_MS) { }
+    mics6814() : PollingComponent(DELAY_MS) { }
 
-  float get_setup_priority() const override { return esphome::setup_priority::BUS; }
+    float get_setup_priority() const override { return esphome::setup_priority::BUS; }
 
-  void setup() override {
+    void setup() override {
 
-    Serial.begin(9600);
-    delay(1000);
-    
-    Wire.begin();
-    
-    if(!gas.initialise()){
-        ESP_LOGD("custom", "MICS6814 - Not Initialised");
-        Serial.println("MICS6814 - Not Initialised");
-        while(true) {}
-    }
-    ESP_LOGD("custom", "MICS6814 - Initialised");
-    Serial.println("MICS6814 - Initialised");
+      Serial.begin(9600);
+      delay(1000);
+      Wire.begin();
 
-  }
+      if(!gas.initialise()){
+          ESP_LOGD("custom","MICS6814 - Not Initialised");
+          while(true) {}
+      }
+      ESP_LOGD("custom", "MICS6814 - Initialised");
+}
 
   void update() override {
 
@@ -46,4 +42,44 @@ class mics6814 : public PollingComponent {
     ox_sensor->publish_state(reading.oxidising);
   
   }
+
+};
+
+class HeaterSwitch : public Component, public Switch {
+  public:
+
+    float get_setup_priority() const override { return esphome::setup_priority::LATE; }
+
+    void setup() override {
+      publish_state(true);
+    }
+
+    void write_state(bool state) override {
+      gas.setHeater(state);
+      publish_state(state);
+    }
+};
+
+class LedChannel : public Component, public FloatOutput {
+  public:
+    float get_setup_priority() const override { return esphome::setup_priority::LATE; }
+    void (GasBreakout::*ch_handler)(uint8_t v);
+
+    LedChannel(char chan) {
+      switch(chan)
+      {
+        case 'R': ch_handler=&GasBreakout::setR; break;
+        case 'G': ch_handler=&GasBreakout::setG; break;
+        case 'B': ch_handler=&GasBreakout::setB; break;
+        default:
+          ch_handler=NULL;
+          ESP_LOGD("custom", "no callback");
+      }
+    }
+
+    void write_state(float state) override {
+      uint8_t period = state * 255;
+      if (ch_handler != NULL)
+        (&gas->*ch_handler)(period);
+    }
 };
